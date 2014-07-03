e6119a2031e717981d4c1e36884efdbbd51d5cf2
Vadim Yalo <yalovek@gmail.com>
2014-07-03 13:53:44 +0400

    read epub on fly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -43,56 +43,108 @@ angular.module('mentelinApp')
                   containerXml = $.parseXML(containerFile),
                   $containerXml = $(containerXml);
 
-              console.log(containerXml);
+              // console.log(containerXml);
 
               var fullPath = $containerXml.find('rootfile').attr('full-path'),
                   packageFile = zip.file(fullPath).asText(),
                   packageXml = $.parseXML(packageFile),
                   $packageXml = $(packageXml),
                   mainFolder = fullPath.substr(0, fullPath.lastIndexOf('/')),
-                  ncxHref;
+                  ncxHref,
+                  cssHref;
 
-              console.log(fullPath);
+              // console.log(fullPath);
               console.log(packageXml);
-              console.log(mainFolder);
+              // console.log(mainFolder);
 
               $packageXml.find('manifest').find('item').each(function () {
                 var href = $(this).attr('href'),
-                    extension = href.substr(href.length - 3)
+                    extension = href.substr(href.length - 3);
 
-                if (extension == 'ncx') {
-                  console.log($(this).attr('href'));
+                if (extension === 'ncx') {
+                  // console.log($(this).attr('href'));
 
                   ncxHref = $(this).attr('href');
                 }
               });
+
+              // $packageXml.find('manifest').find('item').each(function () {
+              //   var media = $(this).attr('media-type');
+
+              //   if (media === 'text/css') {
+              //     console.log($(this).attr('href'));
+
+              //     cssHref = $(this).attr('href');
+              //   }
+              // });
+
+              // var cssFile = zip.folder(mainFolder).file(cssHref).asText();
+
+              // console.log(cssFile);
+
+              // $('style').text(cssFile) .appendTo('head');
 
               var tocFile = zip.folder(mainFolder).file(ncxHref).asText(),
                   tocXml = $.parseXML(tocFile),
                   $tocXml = $(tocXml),
                   navPoints = [];
 
-              console.log(tocXml);
+              // console.log(tocXml);
 
               $tocXml.find('navPoint').find('content').each(function () {
                 var src = $(this).attr('src');
 
-                console.log(src);
+                // console.log(src);
 
                 navPoints.push(src);
               });
 
-              var navPointFile = zip.folder(mainFolder).file(navPoints[0]).asText(),
-                  navPointXml = $.parseXML(navPointFile),
-                  $navPointXml = $(navPointXml),
-                  chapter = $navPointXml.find('body').html();
-
-              console.log(navPointXml);
-              console.log(chapter);
-
               $('#readerContent').html('');
 
-              $('#readerContent').html(chapter);
+              for (var i = 0; i < navPoints.length; i++) {
+                var navPointFile = zip.folder(mainFolder).file(navPoints[i]).asText(),
+                    navPointXml = $.parseXML(navPointFile),
+                    $navPointXml = $(navPointXml),
+                    chapter = $navPointXml.find('body').html();
+
+                // console.log(navPointXml);
+                // console.log(chapter);
+
+                $('#readerContent').append(chapter);
+              }
+
+              $('#readerContent').find('img').each(function () {
+                if (this.length !== 0) {
+                  var src = $(this).attr('src');
+
+                  // console.log(this);
+                  // console.log(src);
+
+                  if (src !== '') {
+                    var imgFile = zip.folder(mainFolder).file(src),
+                        imgType = src.substr(src.lastIndexOf('.') + 1);
+
+                    // console.log(imgType);
+
+                    if (imgFile !== null) {
+                      var buffer = imgFile.asArrayBuffer(),
+                          binary = '',
+                          bytes = new Uint8Array(buffer),
+                          len = bytes.byteLength;
+
+                      for (var i = 0; i < len; i++) {
+                        binary += String.fromCharCode(bytes[i]);
+                      }
+
+                      var imgData = window.btoa(binary);
+
+                      // console.log(imgData);
+
+                      $(this).attr('src', 'data:image/' + imgType + ';base64,' + imgData);
+                    }
+                  }
+                }
+              });
 
               $('#modalReader').modal('toggle');
             }
