fd9539dcbc7f18ec1f3d016d9d385bf73a68a9db
Vadim Yalo <yalovek@gmail.com>
2014-10-20 21:25:19 +0400

    clean up

--- app/scripts/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-window.jQuery(function() {
-    window.jQuery('[src$=svg]').svgmagic();
-});
--- app/scripts/services/session.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('mentelinApp')
-  .factory('Session', function ($resource) {
-    return $resource('/api/session/');
-  });
