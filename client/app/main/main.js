465589f10c5d1939f82bfc54df6bfb3263d9de34
Vadim Yalo <yalovek@gmail.com>
2014-10-20 22:21:38 +0400

    restart project

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('readerApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
