b13047a53771e4f702b42b99f39f2eb99da06dde
Vadim Yalo <yalovek@gmail.com>
2014-11-10 23:26:42 +0300

    fix reader and add pages

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,16 +5,20 @@ angular.module('readerApp')
     $scope.readerWrapperWidth = 0;
     $scope.readerWidth = 0;
     $scope.activePage = 0;
+    $scope.pages = 1;
+    $scope.currentPage = 1;
 
     $scope.pagePrev = function () {
-      if ($scope.activePage !== 0) {
-        var left = $scope.activePage + $scope.readerWrapperWidth;
+      var left = $scope.activePage + $scope.readerWrapperWidth;
 
+      if (left <= 0) {
         $scope.activePage = left;
 
-        $('#readerContent').css({
+        $('#reader').css({
           marginLeft: left
         });
+
+        $scope.currentPage -= 1;
       }
     };
 
@@ -27,21 +31,20 @@ angular.module('readerApp')
         $('#reader').css({
           marginLeft: left
         });
+
+        $scope.currentPage += 1;
       }
     };
 
     $scope.bookLayout = function () {
-      var height = $(window).outerHeight(),
-          $reader = $('#reader'),
+      var $reader = $('#reader'),
           $readerWrapper = $('#readerWrapper');
 
+      $scope.activePage = 0;
+      $scope.currentPage = 1;
       $reader.removeAttr('style').hide();
 
-      $readerWrapper.css({
-        top: height * 0.11
-      });
-
-      var windowHeight = $(window).outerHeight() - $(window).outerHeight() * 0.11 - $(window).outerHeight() * 0.22,
+      var windowHeight = $(window).outerHeight() - 200,
           readerHeight = $reader.outerHeight(),
           column = Math.ceil(readerHeight / windowHeight),
           readerWrapperWidth = $readerWrapper.outerWidth(),
@@ -65,17 +68,19 @@ angular.module('readerApp')
           readerWrapperLines = Math.round(readerWrapperHeight / line),
           lines = ((readerLines - readerWrapperLines) * column);
 
-      column += Math.ceil(lines / readerLines);
-      readerWidth = readerWrapperWidth * column;
-      $scope.readerWidth = readerWidth;
+      if (readerWrapperHeight >= line) {
+        column += Math.ceil(lines / readerLines);
+        readerWidth = readerWrapperWidth * column;
+        $scope.readerWidth = readerWidth;
+        $scope.pages = column;
 
-      $reader.attr('style',
-        'width: ' + readerWidth + 'px;' +
-        '-webkit-column-count: ' + column + ';' +
-        '-moz-column-count: ' + column + ';' +
-        'column-count: ' + column + ';'
-      ).show();
-
+        $reader.attr('style',
+          'width: ' + readerWidth + 'px;' +
+          '-webkit-column-count: ' + column + ';' +
+          '-moz-column-count: ' + column + ';' +
+          'column-count: ' + column + ';'
+        ).show();
+      }
     };
 
     $(function () {
@@ -84,5 +89,6 @@ angular.module('readerApp')
 
     $(window).resize(function () {
       $scope.bookLayout();
+      $scope.$apply();
     });
   });
